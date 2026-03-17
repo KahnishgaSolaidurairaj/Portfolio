948befe57d5e57b6bd3e5956c38fc488344c30ce
KahnishgaSolaidurairaj <krishnakani19@gmail.com>
2026-03-17 13:26:16 -0500

    fixed link

--- src/data/projectInfo.ts
+++ src/data/projectInfo.ts
@@ -60,7 +60,7 @@ export const projects: Project[] = [
     tags: ["Unity 3D", "Multiplayer", "Game Development", "Computer Architecture", "Educational"],
     github: "https://github.com/KahnishgaSolaidurairaj/Maze.exe",
     demo: null,
-    report: "https://drive.google.com/file/d/1UzveTSlzfD2PiqgR8-pEBYF03L8Kdb_e/view?usp=sharing",
+    report: "https://drive.google.com/file/d/1qNl-Djtrz3hFJQFaidGrUGMNRqy4wyJ4/view?usp=sharing",
     features: [
       "Multiplayer cooperative gameplay with host and client interaction",
       "Maze exploration environment representing the inside of a computer",
